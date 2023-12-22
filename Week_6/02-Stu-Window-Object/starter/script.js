b8fb816134ed03c0c5f6c45f07ae0ca1db473b5e
IgorTraspadini <igorchieppe@hotmail.com>
2023-12-22 07:35:35 -0300

    Week_6 02-Stu-Window-Object done!

---
+++ Week_6/02-Stu-Window-Object/starter/script.js
@@ -0,0 +1,16 @@
+// TODO: What will the following line of code log?
+// Global window object
+console.log(window);
+
+// TODO: What will the following line of code log?
+// A representation of the HTML document
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+// The document's highest-level element (<html>)
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// The <head> element
+console.log(document.head);
+
