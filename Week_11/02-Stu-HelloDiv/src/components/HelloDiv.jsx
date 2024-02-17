734e232e233e78ceac5d459feda1b5e4ec6001f1
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-17 08:12:27 -0300

    Week_11 React 02-Stu-HelloDiv done!

---
+++ Week_11/02-Stu-HelloDiv/src/components/HelloDiv.jsx
@@ -0,0 +1,13 @@
+function HelloDiv() {
+  const name = "Igor"
+  return <>
+    <h1>Hello! My name is {name}</h1>
+    <ul>
+      <li>Technology</li>
+      <li>Fishing</li>
+      <li>Take a good time in family</li>
+    </ul>
+  </>;
+}
+
+export default HelloDiv;
